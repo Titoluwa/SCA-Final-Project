74fb8f651b1c338a6d62071f107702737d3d5832
Titoluwa <titoluwaranti@gmail.com>
2022-09-16 09:25:10 +0100

    Welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,18 +3,19 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name') }}</title>
+        <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
-
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-white dark:bg-green-900 sm:items-center py-4 sm:pt-0">
